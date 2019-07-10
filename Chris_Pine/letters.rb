69d7e462020469d418cd2abfb2e5b500183dccdf
SANPAK Ng <pakeng369@gmail.com>
2019-07-09 23:04:04 -0400

    letter.rb

--- Chris_Pine/letters.rb
+++ Chris_Pine/letters.rb
@@ -1,2 +1,13 @@
 "hello"
 'Ruby rocks'
+
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+puts 'I like' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+# puts '12' + 12
+# puts 'Betty' + 12
+puts '2' * 5
+puts 'You\'re swell!'
