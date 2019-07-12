bbf5e25fc24762b38e40a84642e3f70317080baa
SANPAK Ng <pakeng369@gmail.com>
2019-07-11 22:36:46 -0400

    trying some mix it up

--- Chris_Pine/mix_it_up.rb
+++ Chris_Pine/mix_it_up.rb
@@ -1,2 +1,12 @@
 puts '5'
 puts 'mix_it_up'
+puts '15'.to_f
+puts '99.999'.to_f
+
+puts '99.999'.to_i
+puts ''
+
+puts '5 is my favorite number!'.to_i
+puts "2 iasdjaisjda".to_i
+
+puts "Who asked you about 5 or whatever?".to_i
