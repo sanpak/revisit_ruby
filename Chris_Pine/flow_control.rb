563e2b26607dc557b24ead25f0d22426da72b1a9
SANPAK Ng <pakeng369@gmail.com>
2019-07-14 18:53:46 -0400

    7th techer

--- Chris_Pine/flow_control.rb
+++ Chris_Pine/flow_control.rb
@@ -7,9 +7,37 @@
 # puts 'cat' > 'CAT'
 # puts 'cat' < 'CAT'
 
-puts 'Hello, what\'s your name?'
+# puts 'Hello, what\'s your name?'
+# name = gets.chomp
+# puts 'Hello, ' + name + '.'
+# if name == 'Chris'
+#   puts 'What a lovely name!'
+# end
+
+#bracnhing
+# puts 'I am a fortune-teller. Tell me your name:'
+# name = gets.chomp
+#
+# if name == 'Chris'
+#   puts 'I see great things in your future.'
+# else
+#   puts 'Your future is ... Oh my! Look at the time!'
+#   puts 'I really have to go, sorry!'
+# end
+
+puts 'Hello, and welcome to 7th grade English.'
+puts 'My name is Mrs. Gabbard. And your name is...?'
 name = gets.chomp
-puts 'Hello, ' + name + '.'
-if name == 'Chris'
-  puts 'What a lovely name!'
+if name == name.capitalize
+  puts 'Please take a seat, ' + name + '.'
+else
+  puts name + '? You mean ' + name.capitalize + ', right?'
+  puts 'Don\'t you even know how to spell your name??'
+  reply = gets.chomp
+
+  if reply.downcase == 'yes'
+    puts 'Hmmph! Well, sit down!'
+  else
+    puts 'GET OUT!!'
+  end
 end
