46076457e33640c599e87001d490acc124f1e94b
SANPAK Ng <pakeng369@gmail.com>
2019-07-14 18:39:15 -0400

    branching

--- Chris_Pine/flow_control.rb
+++ Chris_Pine/flow_control.rb
@@ -1,4 +1,15 @@
 
 
-puts 1 > 2
-puts 1 < 2
+# puts 1 > 2
+# puts 1 < 2
+#
+# puts 'CAT' < 'cat'
+# puts 'cat' > 'CAT'
+# puts 'cat' < 'CAT'
+
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+if name == 'Chris'
+  puts 'What a lovely name!'
+end
