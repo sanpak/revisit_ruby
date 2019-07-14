2291ea373b224dc52b42cdb41f7fa3d905437f92
SANPAK Ng <pakeng369@gmail.com>
2019-07-14 15:13:53 -0400

    table of contents

--- Chris_Pine/more_about_method.rb
+++ Chris_Pine/more_about_method.rb
@@ -14,6 +14,24 @@
 
 #no.1
 #Angry boss
-puts "What do you want"
-want = gets.chomp
-puts "WHADDAYA MEAN " + '"' + want + '."' + "?!?" + "   YOU'RE FIRED!!"
+# puts "What do you want"
+# want = gets.chomp
+# puts "WHADDAYA MEAN " + '"' + want + '."' + "?!?" + "   YOU'RE FIRED!!"
+
+#no.2
+#Table of contents display
+def table_of_contents
+  lineWidth = 50
+  table_name = "Table of Contents"
+  colume1_label = "Chapter"
+  colume2_label = ""
+  colume3_label = "page"
+  spacer = ""
+  puts table_name.center(lineWidth)
+  puts spacer
+  puts (colume1_label + " 1:").ljust(lineWidth/4) + "Numbers".ljust(lineWidth/4) + (colume3_label+" 1").rjust(lineWidth/2)
+  puts (colume1_label + " 2:").ljust(lineWidth/4) + "Letters".ljust(lineWidth/4) + (colume3_label+" 72").rjust(lineWidth/2)
+  puts (colume1_label + " 3:").ljust(lineWidth/4) + "Variables".ljust(lineWidth/4) + (colume3_label+" 118").rjust(lineWidth/2)
+
+end
+table_of_contents
