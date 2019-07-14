75e9018d5f00528475be727974cc4f90e93ae85c
SANPAK Ng <pakeng369@gmail.com>
2019-07-14 14:45:11 -0400

    Angry boss program

--- Chris_Pine/more_about_method.rb
+++ Chris_Pine/more_about_method.rb
@@ -1,12 +1,19 @@
-lineWidth = 50
+# lineWidth = 50
+#
+# puts ("Old Mother Hubbard".center(lineWidth))
+# puts ("Sat in her cupboard".center(lineWidth))
+# puts ("Eating hercurds an whey,".center(lineWidth))
+#
+# lineWidth = 40
+# str = '--> text <--'
+# puts str.ljust(lineWidth)
+# puts str.rjust(lineWidth)
+# puts str.center(lineWidth)
+# puts str.ljust(lineWidth/2) + str.center(lineWidth/2)+ str.rjust(lineWidth/2)
+#A few things to try
 
-puts ("Old Mother Hubbard".center(lineWidth))
-puts ("Sat in her cupboard".center(lineWidth))
-puts ("Eating hercurds an whey,".center(lineWidth))
-
-lineWidth = 40
-str = '--> text <--'
-puts str.ljust(lineWidth)
-puts str.rjust(lineWidth)
-puts str.center(lineWidth)
-puts str.ljust(lineWidth/2) + str.center(lineWidth/2)+ str.rjust(lineWidth/2)
+#no.1
+#Angry boss
+puts "What do you want"
+want = gets.chomp
+puts "WHADDAYA MEAN " + '"' + want + '."' + "?!?" + "   YOU'RE FIRED!!"
