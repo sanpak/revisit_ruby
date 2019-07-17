b504dd188cce4779dae6681cdc4bbe41f003c905
SANPAK Ng <pakeng369@gmail.com>
2019-07-17 09:01:03 -0400

    99 bottles on the wall lyrics

--- Chris_Pine/flow_control.rb
+++ Chris_Pine/flow_control.rb
@@ -1,43 +1,79 @@
-
-
-# puts 1 > 2
-# puts 1 < 2
 #
-# puts 'CAT' < 'cat'
-# puts 'cat' > 'CAT'
-# puts 'cat' < 'CAT'
-
-# puts 'Hello, what\'s your name?'
-# name = gets.chomp
-# puts 'Hello, ' + name + '.'
-# if name == 'Chris'
-#   puts 'What a lovely name!'
-# end
-
-#bracnhing
-# puts 'I am a fortune-teller. Tell me your name:'
-# name = gets.chomp
 #
-# if name == 'Chris'
-#   puts 'I see great things in your future.'
+# # puts 1 > 2
+# # puts 1 < 2
+# #
+# # puts 'CAT' < 'cat'
+# # puts 'cat' > 'CAT'
+# # puts 'cat' < 'CAT'
+#
+# # puts 'Hello, what\'s your name?'
+# # name = gets.chomp
+# # puts 'Hello, ' + name + '.'
+# # if name == 'Chris'
+# #   puts 'What a lovely name!'
+# # end
+#
+# #bracnhing
+# # puts 'I am a fortune-teller. Tell me your name:'
+# # name = gets.chomp
+# #
+# # if name == 'Chris'
+# #   puts 'I see great things in your future.'
+# # else
+# #   puts 'Your future is ... Oh my! Look at the time!'
+# #   puts 'I really have to go, sorry!'
+# # end
+#
+# puts 'Hello, and welcome to 7th grade English.'
+# puts 'My name is Mrs. Gabbard. And your name is...?'
+# name = gets.chomp
+# if name == name.capitalize
+#   puts 'Please take a seat, ' + name + '.'
 # else
-#   puts 'Your future is ... Oh my! Look at the time!'
-#   puts 'I really have to go, sorry!'
+#   puts name + '? You mean ' + name.capitalize + ', right?'
+#   puts 'Don\'t you even know how to spell your name??'
+#   reply = gets.chomp
+#
+#   if reply.downcase == 'yes'
+#     puts 'Hmmph! Well, sit down!'
+#   else
+#     puts 'GET OUT!!'
+#   end
 # end
 
-puts 'Hello, and welcome to 7th grade English.'
-puts 'My name is Mrs. Gabbard. And your name is...?'
-name = gets.chomp
-if name == name.capitalize
-  puts 'Please take a seat, ' + name + '.'
-else
-  puts name + '? You mean ' + name.capitalize + ', right?'
-  puts 'Don\'t you even know how to spell your name??'
-  reply = gets.chomp
+#looping
 
-  if reply.downcase == 'yes'
-    puts 'Hmmph! Well, sit down!'
-  else
-    puts 'GET OUT!!'
-  end
-end
+# command = ''
+#
+# while command != 'bye'
+#   puts command
+#   command = gets.chomp
+# end
+#
+# puts 'Come again soon!'
+# iAmChris = true
+# iAmPurple = false
+# iLikeFood = true
+# iEatRocks = false
+#
+# puts iAmChris && iLikeFood
+# puts iLikeFood && iEatRocks
+# puts iAmPurple && iLikeFood
+# puts iAmPurple && iEatRocks
+#
+# puts
+# puts iAmChris || iLikeFood
+# puts iLikeFood || iEatRocks
+# puts iAmPurple || iLikeFood
+# puts iAmPurple || iEatRocks
+# puts
+# puts !iAmPurple
+i = 99
+ while i != 1
+   puts " #{i} bottles of beer on the wall, #{i} bottles of beer. Take one down and pass it around, #{i-1} bottles of beer on the wall. "
+   i -= 1
+ end
+
+ puts "1 bottles of beer on the wall, 1 bottles of beer. Take one down and pass it around, no more bottles of beer on the wall."
+ puts "no more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
