c04dc9916cded4652daea607b1ae9ac30aa81cb0
SANPAK Ng <pakeng369@gmail.com>
2019-07-22 22:23:21 -0400

    bubble sort implementation

--- Chris_Pine/array.rb
+++ Chris_Pine/array.rb
@@ -50,4 +50,53 @@ def sort_input_words_v2
   end
   p word_array.sort!
 end
-sort_input_words_v2
+# sort_input_words_v2
+def sort_input_words_v3
+  word_array = []
+  puts "Type in your words"
+  while true
+    word = gets.chomp
+    break if word == ""
+    word_array << word
+  end
+
+end
+
+def my_own_sort(words)
+  i = 0
+  sorted = false
+  while sorted != true
+    i = 0
+    while i < words.length - 1
+      if words[i] > words[i+1]
+        words[i],words[i+1] = words[i+1],words[i]
+        sorted = true
+      else
+        sorted = false
+      end
+      i += 1
+    end
+  end
+  return words
+end
+
+# puts my_own_sort(["apple","john"])
+def test_my_own_sort(words)
+  i = 0
+  k = 0
+  while k < words.length - 1
+    i = 0
+    swapped = false
+    while i < (words.length - k) - 1
+      if words[i] > words[i+1]
+        words[i],words[i+1] = words[i+1],words[i]
+        swapped = true
+      end
+      i += 1
+    end
+    k += 1
+    break if swapped == false
+  end
+  return words
+end
+puts test_my_own_sort(["banana","apple","john","cat","dog","cd"])
