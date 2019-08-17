9392b03917c7f64e2f1c6b59960474fc4fae4394
SANPAK Ng <pakeng369@gmail.com>
2019-08-17 12:22:30 -0400

    selection sort recursion

--- Chris_Pine/selection_sort.rb
+++ Chris_Pine/selection_sort.rb
@@ -17,4 +17,25 @@ def selection_sort(array)
   end
   return array
 end
-puts selection_sort([20,8,4,5,3,20,11,100,40,20,19])
+# puts selection_sort([20,8,4,5,3,20,11,100,40,20,19])
+
+def selection_sort_recursive(array,n = 0)
+  if n == array.length
+    return array
+  end
+  i = n
+  smallest_value = array[n]
+  smallest_idx = n
+  while i < array.length - 1
+    if smallest_value > array[i + 1]
+      smallest_value = array[i + 1]
+      smallest_idx = i + 1
+    end
+    i += 1
+  end
+  array[n],array[smallest_idx] = array[smallest_idx],array[n]
+  selection_sort_recursive(array,n + 1)
+end
+
+p selection_sort_recursive([5,3,4,2,1])
+p selection_sort_recursive([20,8,4,5,3,20,11,100,40,20,19])
