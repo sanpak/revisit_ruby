7dfd5d260dc8d7dfe34486f61df646e6381ef8d3
SANPAK Ng <pakeng369@gmail.com>
2019-08-17 21:52:29 -0400

    selection sort recursion version 2 two 2 arrays

--- Chris_Pine/selection_sort.rb
+++ Chris_Pine/selection_sort.rb
@@ -39,3 +39,24 @@ end
 
 p selection_sort_recursive([5,3,4,2,1])
 p selection_sort_recursive([20,8,4,5,3,20,11,100,40,20,19])
+
+def selection_sort_recursive_v2(array,sorted_array = [])
+  if array.length == 0
+    return sorted_array
+  end
+
+  i = 0
+  smallest_idx = array[i]
+  smallest_value = array[i]
+  j = i
+  while i < array.length
+    if smallest_value > array[i + 1]
+      smallest_value = array[i + 1]
+      smallest_idx = i + 1
+    end
+    i += 1
+  end
+  array[j],array[smallest_idx] = array[smallest_idx],array[j]
+  sorted_array << array.shift
+  selection_sort_recursive_v2(array,sorted_array)
+end
