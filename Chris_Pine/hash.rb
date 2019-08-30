522ca191a2b3f666efa0d805e591d6f19c8884a0
SANPAK Ng <pakeng369@gmail.com>
2019-08-29 22:46:52 -0400

    Hashing

--- Chris_Pine/hash.rb
+++ Chris_Pine/hash.rb
@@ -1,12 +1,30 @@
 
-colorArray = []
-colorHash = {}
+# colorArray = []
+# colorHash = {}
+#
+# colorArray[0] = "red"
+# colorArray[1] = "green"
+# colorArray[2] = "blue"
+# colorHash['string'] = 'red'
+# colorHash['number'] = 'green'
+# colorHash['keywords'] = 'blue'
+# p colorArray
+# p colorHash
+#
+# colorArray.each do |color|
+#   puts color
+# end
+#
+# colorHash.each do |codeType, color|
+#   puts codeType + ': ' + color
+# end
+#
+weirdHash = Hash.new
 
-colorArray[0] = "red"
-colorArray[1] = "green"
-colorArray[2] = "blue"
-colorHash['string'] = 'red'
-colorHash['number'] = 'green'
-colorHash['keywords'] = 'blue'
-p colorArray
-p colorHash
+weirdHash[12] = 'monkeys'
+weirdHash[[]] = 'emptiness'
+weirdHash[Time.new] = 'no time like the present'
+
+weirdHash.each do |typeName,type|
+  puts typeName.to_s + ": " + type
+end
