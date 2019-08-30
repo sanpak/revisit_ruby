d7a372f0f6a15a29e86aff4b65f8da1e8cfe54a0
SANPAK Ng <pakeng369@gmail.com>
2019-08-29 21:54:20 -0400

    hash

---
+++ Chris_Pine/hash.rb
@@ -0,0 +1,12 @@
+
+colorArray = []
+colorHash = {}
+
+colorArray[0] = "red"
+colorArray[1] = "green"
+colorArray[2] = "blue"
+colorHash['string'] = 'red'
+colorHash['number'] = 'green'
+colorHash['keywords'] = 'blue'
+p colorArray
+p colorHash
