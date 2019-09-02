03fc42a7fc1b64d10fcb45705318053cbe418493
SANPAK Ng <pakeng369@gmail.com>
2019-09-02 15:38:14 -0400

    regex

---
+++ Chris_Pine/regex.rb
@@ -0,0 +1,13 @@
+# x = "This is a test"
+# y = "This123 a 34test 34."
+# puts x.sub(/^../,"Hello")
+#
+# puts x.sub(/..$/,"Hello")
+#
+# puts x.sub(/...$/,"begin")
+# x.scan(/./) { |letter| puts letter}
+# x.scan(/\w\w/) { |letter| puts letter}
+# puts
+# y.scan(/\w\w/) { |letter| puts letter}
+# puts
+# y.scan(/../) { |letter| puts letter}
